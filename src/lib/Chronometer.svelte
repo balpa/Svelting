13498817fd23f12b1d28b795343a95309bc550df
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-12-27 11:32:54 +0300

    seems smooth

---
+++ src/lib/Chronometer.svelte
@@ -0,0 +1,21 @@
+<script>
+  let seconds = 0;
+  let chrono;
+
+  function start() {
+    chrono = setInterval(() => {
+      seconds += 0.1;
+    }, 100);
+  }
+  function stop() {
+    clearInterval(chrono);
+    seconds = 0;
+  }
+</script>
+
+<div>
+  <h1>Stopwatch</h1>
+  <button on:click={start}> start </button>
+  <button on:click={stop}> stop </button>
+  <h1>{seconds.toFixed(1)}</h1>
+</div>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,22 @@
 <script>
-  let count = 0
+  let count = 0;
+
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
+
+  const decrement = () => {
+    count -= 1;
+  };
+
+  const reset = () => {
+    count = 0;
+  };
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<button on:click={increment}> increment </button>
+<button on:click={decrement}> decrement</button>
+
+<button on:click={reset}> reset </button>
+
+<h1>{count}</h1>
