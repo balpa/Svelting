d1012da4a5fd86dba2031c6fefd88c21d5baa1eb
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-12-28 11:25:50 +0300

    passing props

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,48 @@
 <script>
   import Chronometer from "./lib/Chronometer.svelte";
   import Counter from "./lib/Counter.svelte";
+  import Header from "./lib/Header.svelte";
+
+  export let enteredName = "";
+  export let isEntered = false;
+
+  function enter() {
+    isEntered = true;
+  }
+  function handleInputOnchange(target) {
+    enteredName = target.value;
+  }
 </script>
 
 <main>
-  <div class="card">
-    <Counter />
-    <Chronometer />
-  </div>
+  {#if !isEntered}
+    <div class="enterance">
+      <input
+        type="text"
+        value={enteredName}
+        id="name-input"
+        placeholder="Enter name"
+        on:change={(e) => handleInputOnchange(e.target)}
+      />
+      <button on:click={enter}>Enter</button>
+    </div>
+  {/if}
+
+  {#if isEntered}
+    <div class="card">
+      <Header {enteredName} />
+      <Counter />
+      <Chronometer />
+    </div>
+  {/if}
 </main>
 
 <style>
+  .enterance {
+    display: flex;
+    flex-direction: column;
+  }
+  button {
+    margin-top: 10px;
+  }
 </style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let enteredName;
+  let headerTitle = `Hi, ${enteredName.toLowerCase()}!`;
+</script>
+
+<div>
+  <h1>{headerTitle}</h1>
+</div>
