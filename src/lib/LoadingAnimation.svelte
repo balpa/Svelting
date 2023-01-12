e2c7fa0b9b01f8aebb28afa1a4d7e51de5c2aee4
Berke Altıparmak <berkealtiparmak@outlook.com>
2023-01-12 17:29:40 +0300

    loading anim diff appr

--- src/lib/LoadingAnimation.svelte
+++ src/lib/LoadingAnimation.svelte
@@ -1,33 +1,39 @@
 <script>
-  let loadingBars;
-  console.log(loadingBars);
+  import { onDestroy } from "svelte";
+
+  let progress = 0;
+
+  function getProgressColor(progress) {
+    var r = 255;
+    var g = Math.round(255 * (1 - progress));
+    var b = 0;
+    console.log("rgb(" + r + "," + g + "," + b + ")");
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
+  let intervalId = setInterval(() => {
+    progress = (progress + 0.01) % 1;
+  }, 10);
+
+  onDestroy(() => clearInterval(intervalId));
+  export let progressColor = getProgressColor(progress);
 </script>
 
-<div id="loading-container">
-  <div class="loading-bar" bind:this={loadingBars}>1</div>
-  <div class="loading-bar">2</div>
-  <div class="loading-bar">3</div>
-  <div class="loading-bar">4</div>
-  <div class="loading-bar">5</div>
-  <div class="loading-bar">6</div>
-  <div class="loading-bar">7</div>
-  <div class="loading-bar">8</div>
-  <div class="loading-bar">9</div>
-  <div class="loading-bar">10</div>
+<div class="loading-bar-container">
+  <div
+    class="loading-bar"
+    style="width: 100%; background-color: {progressColor}"
+  />
 </div>
 
 <style>
-  #loading-container {
-    width: 40px;
-    height: 70px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-  }
-  .loading-bar {
+  .loading-bar-container {
     width: 100%;
-    height: 3px;
-    background-color: white;
-    border-radius: 5px;
+    height: 20px;
+  }
+
+  .loading-bar {
+    height: 100%;
+    transition: width 0.5s linear;
   }
 </style>
